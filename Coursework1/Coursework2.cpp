3c34b4ad8b7eef03f9f15a65dbd03d5c8f00f1af
Herman <Herma@DESKTOP-KL642PV>
2024-01-10 17:43:06 +0200

    item adding and printing of datastructure

--- Coursework1/Coursework2.cpp
+++ Coursework1/Coursework2.cpp
@@ -19,57 +19,115 @@ public:
 	HEADER_E* pStruct;
 public:
 	DataStructure();
-	DataStructure(char *pFilename);
-	~DataStructure();
-	DataStructure(const DataStructure &original);
-public:
-	void PrintDataStructure(HEADER_E *pStruct);
+	//DataStructure(char *pFilename);
+	friend ostream& operator<<(ostream& ostr, const DataStructure& str);
+	void operator+=(ITEM10* pNewItem);
+	//~DataStructure();
+	//DataStructure(const DataStructure &original);
 };
 
 DataStructure::DataStructure() {
-	pStruct = GetStruct5(10, 30);
+	pStruct = new HEADER_E [26]();
 }
 
-
-DataStructure::DataStructure(char* pFilename) {
-
-}
-
-
-DataStructure::~DataStructure() {
-
-}
-
-
-DataStructure::DataStructure(const DataStructure &original) {
-
-}
-
-void DataStructure::PrintDataStructure(HEADER_E *pStruct){
-	HEADER_E* pStructTemp = pStruct;
-	int i = 1;
-	while (pStructTemp != NULL) {
-		cout << "HEADER:" << pStructTemp->cBegin << "\n";
-		void** ppItem = pStructTemp->ppItems;
-		for (int j = 0; j < 25; j++) {
-			ITEM10* pItem = (ITEM10*)ppItem[j];
-			while (pItem != NULL) {
-				cout << " | " << i << ") " << pItem->pID << pItem->Code;
-				cout << " " << pItem->Date.Day << "/" << pItem->Date.pMonth << "/" << pItem->Date.Year << "\n";
-				pItem = pItem->pNext;
-				i++;
+ostream& operator<<(ostream& ostr, const DataStructure& str) {
+	HEADER_E* pHeader = str.pStruct;
+	if (!str.pStruct) {
+		cout << "Data Structure is empty!" << endl;
+		return ostr;
+	}
+	while (pHeader != nullptr) {
+		if (pHeader->ppItems != nullptr) {
+			for (int i = 0; i < 26; i++) {
+				//ITEM10* pItem = reinterpret_cast<ITEM10*>(pHeader->ppItems[i]);
+				ITEM10* pItem = (ITEM10*)pHeader->ppItems[i];
+				while (pItem != nullptr) {
+					ostr << i++ << " )" << pItem->pID << "|" << pItem->Code << "|";
+					ostr << pItem->Date.Day << "/" << pItem->Date.pMonth << "/" << pItem->Date.Year << endl;
+					pItem = pItem->pNext;
+				}
 			}
 		}
-		pStructTemp = pStructTemp->pNext;
+		pHeader = pHeader->pNext;
 	}
+	return ostr;
 }
 
+char FindSecondLetter(char* pNewItemID) {
+	char* pSecondId = (char*)' ', secondChar;
+	char* pSpace = strchr(pNewItemID, ' '); // find the second word of ID
+	if (!pSpace) {
+		return 0;
+	}
+	pSecondId = pSpace + 1;
+	secondChar = *(pSecondId);
+	return secondChar;
+}
+
+void DataStructure::operator+=(ITEM10* pNewItem) {
+	if (!pNewItem || !pNewItem->pID) {
+		cout << "Invalid item or ID" << endl;
+		return;
+	}
+
+	pNewItem->pNext = NULL;
+	char firstChar = *(pNewItem->pID);
+	char secondChar = FindSecondLetter(pNewItem->pID);
+	 
+	HEADER_E** ppCurrentStruct = &pStruct;
+	while (*ppCurrentStruct && (*ppCurrentStruct)->cBegin != firstChar) {
+		ppCurrentStruct = &(*ppCurrentStruct)->pNext;
+	}
+	//if header is not found
+	if (*ppCurrentStruct == NULL) {
+		*ppCurrentStruct = new HEADER_E{
+			new void* [26](),
+			firstChar,
+			NULL
+		};
+	}
+	//create new pointer to correct header
+	HEADER_E* pNewStruct = *ppCurrentStruct;
+
+	int index = secondChar - 'A';
+	ITEM10** ppItem = reinterpret_cast<ITEM10**>(&pNewStruct->ppItems[index]);
+
+	//check if item already exists
+	for (ITEM10* pItem = *ppItem; pItem != nullptr; pItem = pItem->pNext) {
+		if (strcmp(pItem->pID, pNewItem->pID) == 0) {
+			cout << "Item " << pNewItem->pID << " exists!" << endl;
+		}
+	}
+
+	pNewItem->pNext = *ppItem;
+	*ppItem = pNewItem;
+}
+
+
+//DataStructure::DataStructure(char* pFilename) {
+//
+//}
+//
+//
+//DataStructure::~DataStructure() {
+//
+//}
+//
+//
+//DataStructure::DataStructure(const DataStructure &original) {
+//
+//}
 
 
 int main(){
-	DataStructure pStructure;
-
-	pStructure.PrintDataStructure(pStructure.pStruct);
+	cout << " TEST 1 " << endl;
+	DataStructure pDataStructure;
+	cout << pDataStructure << endl << endl;
+	for (int i = 0; i < 10; i++) {
+		ITEM10* item = (ITEM10*)GetItem(10);
+		pDataStructure += item;
+	}
+	cout << pDataStructure << endl << endl;
 
 	return 0;
 }
